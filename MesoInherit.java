f6257aed33b3268df9b752bb53d52db0da4c75c7
Chris Bharucha <christopher.k.bharucha-1@ou.edu>
2019-10-05 20:19:53 -0500

    writing part of MesoInherit

--- MesoInherit.java
+++ MesoInherit.java
@@ -5,7 +5,8 @@ import java.io.IOException;
 public class MesoInherit extends MesoAbstract
 {
 	private String stID; //creating a class variable for the passed in stID
-	private String[] stationData = new String[10];
+	private String[] stationData = new String[10]; //array that stores all station ID's
+	private int numStations = 0; //counter for the stationData array starting at zero
 	
 	/*
 	 * This constructor takes in an ID input as a parameter and uses a reader to extract the data from 
@@ -14,17 +15,24 @@ public class MesoInherit extends MesoAbstract
 	public MesoInherit(String stID) throws IOException {
 		this.stID = stID; //updating class variable with parameter ID
 		
-		//creating the bufferedreader to read in the file
+		//creating the BufferedReader to read in the file
 		BufferedReader br = new BufferedReader(new FileReader("Mesonet.txt"));
 		for (int i = 0; i < 3; ++i) {
-			br.readLine();	//reading through first 3 lines of the file that dont contain data
+			br.readLine();	//reading through first 3 lines of the file that don't contain data
 		}
-		String dataline = br.readLine(); //first line that actually contains data
-		while (br.ready()) {
-			
-		}
-		
-		
+		String dataLine = br.readLine(); //first line that actually contains data stored in String
+		while (br.ready()) { //while file still has data to be read
+			if (numStations == stationData.length) {
+				expandArray(); //expanding the array if capacity is full
+			}
+			/*
+			 *splitting the dataLine String by white spaces and storing the second element into info because
+			 *the first index is a white space
+			*/
+			String info = dataLine.split("\\s+")[1];
+			stationData[numStations++] = info; //storing all the stID's into the stationData array
+			dataLine = br.readLine(); //reading the next line for the next iteration of the loop
+		} br.close(); //closing the reading when finished
 	}
 	
 	/*
@@ -37,4 +45,21 @@ public class MesoInherit extends MesoAbstract
 			newArray[i] = stationData[i];	//copying over the old array contents to new array
 		} this.stationData = newArray;	//updating class reference
 	}
+	
+	
+	/*
+	 * This method calculates the ASCII Ceiling, ASCII Floor, and the ASCII Average
+	 */
+	public int[] calAverage() {
+		return null;
+	}
+	
+	/*
+	 * This method finds the letter average using ASCII values
+	 */
+	public char letterAverage() {
+		return 'x';
+	}
+	
 }
+
