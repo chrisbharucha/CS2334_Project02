fcd00b9a5640a65a64a0245c616b03ddda1776d7
Chris Bharucha <christopher.k.bharucha-1@ou.edu>
2019-10-06 08:14:38 -0500

    setting up methods to be completed

--- LetterAvg.java
+++ LetterAvg.java
@@ -1,4 +1,15 @@
 
 public class LetterAvg {
-
+	
+	public LetterAvg(char c) {
+		
+	}
+	
+	public int numberOfStationWithLetterAvg() {
+		return 0;
+	}
+	
+	public String toString() {
+		return String.format("They are: \n %s \n %s \n %s \n %s", args);
+	}
 }
