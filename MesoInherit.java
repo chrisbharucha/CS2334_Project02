6befe66588e15130e7d2d9a4928457722fe0889a
Chris Bharucha <christopher.k.bharucha-1@ou.edu>
2019-10-05 21:19:12 -0500

    got all the ascii values

--- MesoInherit.java
+++ MesoInherit.java
@@ -1,9 +1,17 @@
 
 public class MesoInherit extends MesoAbstract
 {
-	private int[] ascii; //array that contains the ASCII values of letters
+	private int[] ascii = new int[26]; //array that contains the ASCII values of letters
+	
+	/*
+	 * This constructor takes in a MesoStation object as a parameter and also fills in the 
+	 * ASCII values in the ascii array
+	 */
 	public MesoInherit(MesoStation stID){
-		
+		int startingVal = 65;
+		for (int i = 0; i < ascii.length; ++i) { //using a for loop to fill in the ASCII values
+			ascii[i] = startingVal++;
+		}
 	}
 	
 	/*
